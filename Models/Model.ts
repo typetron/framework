7d81d330db7aeb4c507f9c3bc2fcb8fa6a3cca6d
IonelLupu <wolff_cristy@yahoo.com>
2020-03-22 21:00:39 +0200

    Models: now transforms arrays of objects too

--- Models/Model.ts
+++ Models/Model.ts
@@ -2,15 +2,20 @@ import { Constructor } from '../Support';
 import { ModelField, ModelMetadataKey } from './index';
 
 export class Model {
-    static from<K extends Model, Q>(this: Constructor<K>, entities: Q[]): K[] {
+    static from<T extends (Model | Model[]), Q extends Model>(this: Constructor<Q>, entities: T | T[]): Q | Q[] {
         const fields: {[key: string]: ModelField} = Reflect.getMetadata(ModelMetadataKey, this) || {};
 
-        return entities.map(entity => {
-            const data: { [key in keyof K]?: Q[keyof Q] } = {};
-            Object.keys(fields).forEach(fieldName => {
-                data[fieldName as keyof K] = entity[fieldName as keyof Q];
-            });
-            return data;
-        }) as K[];
+        if (entities instanceof Array) {
+            return entities.map(entity => Model.transform<T, Q>(fields, entity));
+        }
+        return Model.transform<T, Q>(fields, entities);
+    }
+
+    private static transform<T, Q extends Model>(fields: {[p: string]: ModelField}, entity: T): Q {
+        const data: { [key in keyof Q]?: T[keyof T] } = {};
+        Object.keys(fields).forEach(fieldName => {
+            data[fieldName as keyof Q] = entity[fieldName as keyof T];
+        });
+        return data as Q;
     }
 }
